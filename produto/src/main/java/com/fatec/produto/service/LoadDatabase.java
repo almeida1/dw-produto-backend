e6d5994cafcaf5dcb9fe6ca88007a65d25ee4ad2
almeida1 <engsoftware1@gmail.com>
2023-06-13 18:51:38 -0300

    Refatoração - classe loaddatabase

--- produto/src/main/java/com/fatec/produto/service/LoadDatabase.java
+++ produto/src/main/java/com/fatec/produto/service/LoadDatabase.java
@@ -2,6 +2,8 @@ package com.fatec.produto.service;
 
 
 
+import java.util.Arrays;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
@@ -17,8 +19,10 @@ public class LoadDatabase {
 	@Bean
 	CommandLineRunner initDatabase(ProdutoRepository repository) {
 		return args -> {
-			Produto produto = new Produto("Eletrobomba 110V para Maquina de Lavar e Lava Louças", 51.66, 12);
-			repository.save(produto);
+			Produto produto1 = new Produto("Eletrobomba 110V para Maquina de Lavar e Lava Louças", 51.66, 12);
+			Produto produto2 = new Produto("Tirante Original Brastemp E Consul De 7 A 12 Kg 11cm", 3.90,20);
+			Produto produto3 = new Produto("Termoatuador Lavadora Colormaq Electrolux GE", 29.70,40);
+			repository.saveAll(Arrays.asList(produto1, produto2, produto3));
 			logger.info (">>>>> loaddatabase -> registro incluido");
 		};
 	}
